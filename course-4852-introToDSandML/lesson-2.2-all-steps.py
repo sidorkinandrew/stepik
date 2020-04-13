95b1c638445e0734db29f057186e31af0fc26728
git_admin <sidorkina.oksana.19@gmail.com>
2020-04-13 22:19:54 +0000

    Update lesson-2.2-all-steps.py

--- course-4852-introToDSandML/lesson-2.2-all-steps.py
+++ course-4852-introToDSandML/lesson-2.2-all-steps.py
@@ -15,12 +15,38 @@ enthropy_dataset
 def ent(data):
   return entropy(data.Вид.value_counts() / len(data), base=2)
 
+# calc enthropy
 print('Шерстист на 0: ', ent(enthropy_dataset[enthropy_dataset.Шерстист == 0]))
 print('Шерстист на 1: ', ent(enthropy_dataset[enthropy_dataset.Шерстист == 1]))
 print('Гавкает на 0: ', ent(enthropy_dataset[enthropy_dataset.Гавкает == 0]))
 print('Гавкает на 1: ', ent(enthropy_dataset[enthropy_dataset.Гавкает == 1]))
 print('Лазает по деревьям на 0: ', ent(enthropy_dataset[enthropy_dataset['Лазает по деревьям'] == 0]))
 print('Лазает по деревьям на 1: ', ent(enthropy_dataset[enthropy_dataset['Лазает по деревьям'] == 1]))
+
+# calc information gain
+def ig(data, feature):
+  ent0 = ent(data[data[feature] == 0])
+  ent1 = ent(data[data[feature] == 1])
+  n = len(data)
+  n0 = len(data[data[feature] == 0])
+  n1 = len(data[data[feature] == 1])
+  return ent(data) - ent0 * n0 / n - ent1 * n1 / n
+
+# semi-manual
+E = -(4/10)*np.log2(4/10) - (6/10)*np.log2(6/10)
+sh0 = ent(enthropy_dataset[enthropy_dataset.Шерстист == 0])
+sh1 = ent(enthropy_dataset[enthropy_dataset.Шерстист == 1])
+print("Information gain for Шерстист:", round(E - (1/10*sh0+9/10*sh1),2))
+g0 = ent(enthropy_dataset[enthropy_dataset.Гавкает == 0])
+g1 = ent(enthropy_dataset[enthropy_dataset.Гавкает == 1])
+print("Information gain for Гавкает:", round(E - (5/10*g0+5/10*g1),2))
+l0 = ent(enthropy_dataset[enthropy_dataset['Лазает по деревьям'] == 0])
+l1 = ent(enthropy_dataset[enthropy_dataset['Лазает по деревьям'] == 1])
+print("Information gain for Лазает по деревьям:", round(E - (6/10*l0+6/10*l1),2))
+
+# automated
+for afeature in ["Шерстист","Гавкает","Лазает по деревьям"]:
+  print("Information gain for",afeature,"is", round(ig(enthropy_dataset,afeature),2))
 
 # OR alternatively:
 from IPython.display import SVG
